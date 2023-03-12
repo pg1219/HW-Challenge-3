b84ced98665e679ebdced4edec37cb14ae31640a
pg1219 <peter.gronseth@gmail.com>
2023-03-12 00:10:42 -0500

    push

--- Develop/script.js
+++ Develop/script.js
@@ -10,38 +10,61 @@ function writePassword() {
 
 }
 
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" .split("");
-  var lowerCase = "abcdefghijklmnopqrstuvwxyz" . split("");
-  var number = "0123456789" .split("");
-  var specialCharacter = "!@#$%^&" .split("");
-  var lengthpw;
+  var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+  var number = ["0123456789"];
+  var specialCharacter = ["!@#$%^&"];
+  var lengthpw
+  var selectedChar = [];
 
-  var indexu = upperCase[Math.floor(Math.random() * upperCase.length)];
-  var indexl = lowerCase[Math.floor(Math.random() * lowerCase.length)];
-  var indexn = number[Math.floor(Math.random() * number.length)];
-  var indexs = specialCharacter[Math.floor(Math.random() * specialCharacter.length)];
+  // var indexu = upperCase[Math.floor(Math.random() * upperCase.length)];
+  // var indexl = lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  // var indexn = number[Math.floor(Math.random() * number.length)];
+  // var indexs = specialCharacter[Math.floor(Math.random() * specialCharacter.length)];
  
-  {console.log(indexu, indexl, indexn, indexs)};
+  // {console.log(indexu, indexl, indexn, indexs)};
 
 function generatePassword(){
-  lengthpw = prompt("How many characters would you like your password? Please enter between 8 and 128 characters.");
+  lengthpw = prompt("How many characters would you like your password? Please enter between 8 and 128 characters.");{console.log(lengthpw)}
 
   if (!lengthpw) {lengthpw = prompt("Please try again");}
 
   else if (lengthpw < 8 || lengthpw > 128){lengthpw = prompt("Try again. Password must be between 8 and 128 characters.")}
 
   else {
-    upperCaseConfirm = confirm("Include upper case characters in your password?");
+    upperCaseConfirm = confirm("Include upper case characters in your password?")
+    {console.log(upperCaseConfirm)}
     lowerCaseConfirm = confirm("Include lower case characters in your password?");
+    {console.log(lowerCaseConfirm)}
     numberConfirm = confirm("Include numbers in your password?");
-    specialCharacterConfirm = confirm("Include special characters in your password?");
+    {console.log(numberConfirm)}
+    specialCharacterConfirm = confirm("Include special characters in your password?");{console.log(specialCharacterConfirm)}
   }
 
-  if (upperCaseConfirm && lowerCaseConfirm && numberConfirm && specialCharacterConfirm) {
-    for (var i = 0; i < lengthpw; i++) { 
-      password = password.toString()+indexu.toString()+indexl.toString()+indexn.toString()+indexs.toString();
-    }
+  if (upperCaseConfirm == true) {
+    selectedChar += upperCase;
   }
+
+  if (lowerCaseConfirm == true) {
+    selectedChar += lowerCase;
+  }
+
+  if (numberConfirm == true) {
+    selectedChar += number;
+  }
+
+  if (specialCharacterConfirm == true) {
+    selectedChar += specialCharacter
+  }
+  
+  selectedChar = selectedChar.split("").join("");
+
+  console.log(selectedChar)
+
+  // if (upperCaseConfirm && lowerCaseConfirm && numberConfirm && specialCharacterConfirm) {
+  //   for (var i = 0; i < lengthpw; i++) { 
+      
+  // }
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
